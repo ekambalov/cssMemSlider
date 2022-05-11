b9a1b7907f61c80a4fe547c5c7b3cf924934ce21
ekambalov <emakcomp@gmail.com>
2022-05-11 11:29:38 +0200

    fix: fixed area btns

--- cssMemSlider/js/index.js
+++ cssMemSlider/js/index.js
@@ -1,7 +1,10 @@
 const carousel = document.querySelector('.slider__carousel')
 const navBtns = document.querySelectorAll('.slider__btn')
+const navBtnsContainer = document.querySelectorAll('.slider__btnContainer')
 
 let activePage = 0;
+
+
 
 const activateBtn = (number) => {
     navBtns[activePage].classList.remove('slider__btn--active');
@@ -12,7 +15,7 @@ const activateBtn = (number) => {
 activateBtn(activePage)
 
 for (let i = 0; i < 7; i += 1) {
-    navBtns[i].addEventListener('click',()=>{
+    navBtnsContainer[i].addEventListener('click',()=>{
         carousel.style.left = `-${i*620}px`;
         activateBtn(i);
 
