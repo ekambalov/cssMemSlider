40d2e66df8e20aa57a524c42a97f1751262e04e7
ekambalov <emakcomp@gmail.com>
2022-05-11 13:17:49 +0200

    feat: add animation for text

--- cssMemSlider/js/index.js
+++ cssMemSlider/js/index.js
@@ -1,23 +1,45 @@
 const carousel = document.querySelector('.slider__carousel')
 const navBtns = document.querySelectorAll('.slider__btn')
 const navBtnsContainer = document.querySelectorAll('.slider__btnContainer')
+const memeText = document.querySelector('.slider__meme-text')
+
+const memesText = [
+    'жыццёва',
+    'заганяюся і заганяюся',
+    'магу сабе дазволіць',
+    'толькі не крыўдуй',
+    'няма чаго дадаць',
+    'яшчэ скажыце што вам не падабаецца',
+    'такое ўжо жыццё'
+]
 
 let activePage = 0;
+
+const changeText = () => {
+    memeText.textContent = memesText[activePage];
+    memeText.removeEventListener('animationend',changeText);
+    memeText.classList.remove('slider__meme-text--move'); 
+
+}
+
 
 
 
 const activateBtn = (number) => {
+    carousel.style.left = `-${number*620}px`;
     navBtns[activePage].classList.remove('slider__btn--active');
     navBtns[number].classList.add('slider__btn--active');
+    memeText.classList.add('slider__meme-text--move'); 
+    memeText.addEventListener('transitionend',changeText);   
     activePage = number;
 }
 
-activateBtn(activePage)
+navBtns[activePage].classList.add('slider__btn--active');
+
 
 for (let i = 0; i < 7; i += 1) {
-    navBtnsContainer[i].addEventListener('click',()=>{
-        carousel.style.left = `-${i*620}px`;
+    navBtnsContainer[i].addEventListener('click',()=>{        
         activateBtn(i);
-
     })
 }
+
